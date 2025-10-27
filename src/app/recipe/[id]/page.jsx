2fc63d75b79c53eed63978129063e7804fefb3c9
Nick Leal <business.nleal@gmail.com>
2025-10-27 01:48:25 -0700

    Create missing page.jsx and error.jsx files in the /recipe/[id]/ directory

---
+++ src/app/recipe/[id]/page.jsx
@@ -0,0 +1,44 @@
+import Recipe from "@/src/components/Recipe.jsx";
+import { Suspense } from "react";
+import { getRecipeById } from "@/src/lib/firebase/firestore.js";
+import {
+  getAuthenticatedAppForUser,
+  getAuthenticatedAppForUser as getUser,
+} from "@/src/lib/firebase/serverApp.js";
+import ReviewsList, {
+  ReviewsListSkeleton,
+} from "@/src/components/Reviews/ReviewsList";
+import {
+  GeminiSummary,
+  GeminiSummarySkeleton,
+} from "@/src/components/Reviews/ReviewSummary";
+import { getFirestore } from "firebase/firestore";
+
+export default async function Home(props) {
+  const params = await props.params;
+  const { currentUser } = await getUser();
+  const { firebaseServerApp } = await getAuthenticatedAppForUser();
+  const recipe = await getRecipeById(
+    getFirestore(firebaseServerApp),
+    params.id
+  );
+
+  return (
+    <main className="main__recipe">
+      <Recipe
+        id={params.id}
+        initialRecipe={recipe}
+        initialUserId={currentUser?.uid || ""}
+      >
+        <Suspense fallback={<GeminiSummarySkeleton />}>
+          <GeminiSummary recipeId={params.id} />
+        </Suspense>
+      </Recipe>
+      <Suspense
+        fallback={<ReviewsListSkeleton numReviews={recipe.numRatings} />}
+      >
+        <ReviewsList recipeId={params.id} userId={currentUser?.uid || ""} />
+      </Suspense>
+    </main>
+  );
+}
